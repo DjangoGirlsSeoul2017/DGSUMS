efb58fbcdeebbe4ef4a8eaeb0ed61ba8d61c1565
Beomi <latheledusjp@gmail.com>
2017-02-13 00:08:23 +0900

    make default models

--- Events/models.py
+++ Events/models.py
@@ -6,19 +6,26 @@ from django.contrib.auth import get_user_model
 
 # Core
 from Core.models import TimeStampModel
+from Core.models import ImageModel
 
 # Apps
 from Users.models import UserInfo
 
 
 class Event(TimeStampModel):
+    EVENT_TYPES = (
+        (1, 'DjangoGirls Seminar'),
+        (2, 'Community Tutorial'),
+        (3, 'Community Meetup'),
+    )
+
     name = models.CharField(max_length=200)
     content = models.TextField()
     event_start = models.DateField()
     event_end = models.DateField()
     register_fee = models.PositiveIntegerField()
     register_start = models.DateField()
-    register_until = models.DateField()
+    register_end = models.DateField()
     registered_users = models.ManyToManyField(
         settings.AUTH_USER_MODEL, related_name='registered_users'
     )
@@ -26,18 +33,50 @@ class Event(TimeStampModel):
     attended_users = models.ManyToManyField(
         settings.AUTH_USER_MODEL, related_name='attended_users'
     )
+    event_type = models.PositiveSmallIntegerField(choices=EVENT_TYPES)
 
     def __str__(self):
         return self.title
 
     @property
     def can_register(self):
-        if timezone.now() < self.registered_events:
+        if (self.register_start < timezone.now() < self.register_end):
             return True
         return False
 
-    def is_user_registered(self, user_pk):
+    def if_user_registered(self, user_pk):
         user = get_user_model().objects.get(pk=user_pk)
         if user in self.registered_users:
             return True
         return False
+
+
+class EventImage(ImageModel):
+    event = models.ForeignKey(Event)
+
+
+class Study(TimeStampModel):
+    name = models.CharField(max_length=200)
+    content = models.TextField()
+    study_start = models.DateField()
+    study_end = models.DateField()
+    register_fee = models.PositiveIntegerField()
+    register_start = models.DateField()
+    register_end = models.DateField()
+    registered_users = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    @property
+    def study_weeks(self):
+        return ((self.study_end - self.study_start).days / 7) + 1
+
+    def __str__(self):
+        return self.name
+
+
+class StudyTimes(TimeStampModel):
+    study = models.ForeignKey(Study)
+    attended_users = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    times = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.study.name + ': ' + str(self.times) + '회'
--- Users/models.py
+++ Users/models.py
@@ -12,3 +12,20 @@ class UserInfo(TimeStampModel):
 
     def __str__(self):
         return self.user.name
+
+    @property
+    def registered_events(self):
+        return self.user.registered_users.all()
+
+    @property
+    def attended_events(self):
+        return self.user.attended_users.all()
+
+    @property
+    def event_attendance_rate(self):
+        reg_events_num = len(self.registered_events)
+        atnd_events_num = len(self.attended_events)
+        if reg_events_num == 0:
+            return float(1)
+        else:
+            return float(atnd_events_num / reg_events_num)
